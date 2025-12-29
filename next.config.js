6342433526481f4b1a489306400d606379d74bca
opitwwb <Opitweb@gmail.com>
2025-12-29 06:07:52 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['placehold.co', 'opitweb.com.br'],
+  },
+}
+
+module.exports = nextConfig
